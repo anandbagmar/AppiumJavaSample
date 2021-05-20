f0d68c828fdd217b9531ed2a47e52fb082965181
Anand Bagmar <anandbagmar@users.noreply.github.com>
2021-05-20 15:38:26 +0530

    Update setup_mac.sh

--- setup_mac.sh
+++ setup_mac.sh
@@ -59,6 +59,8 @@ pwd
 
 echo "Installing ./sdkmanager tools platform-tools platforms;android-28 build-tools;28.0.3"
 pwd
+touch ~/.android/repositories.cfg
+./sdkmanager "tools" "platform-tools" "platforms;android-28" "build-tools;28.0.3" "info_back"
 ./sdkmanager "tools" "platform-tools" "platforms;android-28" "build-tools;28.0.3"
 
 sleep 5
