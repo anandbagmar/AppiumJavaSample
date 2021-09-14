7e659c26ec4f067950923e2e6daedb8752009dc6
Anand Bagmar <abagmar@gmail.com>
2021-09-14 15:50:06 +0530

    added methods in DriverUtils to support multiple browser types

--- src/test/java/com/eot/utils/DriverUtils.java
+++ src/test/java/com/eot/utils/DriverUtils.java
@@ -7,7 +7,7 @@ import java.io.*;
 import java.util.*;
 
 public class DriverUtils {
-    public static String getPathForChromeDriverFromMachine() {
+    public static String setPathForChromeDriverFromMachine() {
         WebDriverManager.chromedriver().setup();
         String chromeDriverPath = WebDriverManager.chromedriver().getDownloadedDriverPath();
         System.out.println("ChromeDriver path: " + chromeDriverPath);
@@ -15,50 +15,78 @@ public class DriverUtils {
         return chromeDriverPath;
     }
 
-    public static void setChromeDriverForConnectedDevice() {
-        int[] versionNamesArr = getChromeVersionsFor();
+    private static void setDriverForConnectedDevice(String browserName) {
+        int[] versionNamesArr = getBrowserVersionsFor(browserName);
         if (versionNamesArr.length > 0) {
-            int highestChromeVersion = Arrays.stream(versionNamesArr).max().getAsInt();
-            String message = "Setting up ChromeDriver for Chrome version " + highestChromeVersion + " on device";
+            int highestBrowserVersion = Arrays.stream(versionNamesArr).max().getAsInt();
+            String message = "Setting up ChromeDriver for Chrome version " + highestBrowserVersion + " on device";
             System.out.println(message);
-            WebDriverManager.chromedriver().browserVersion(String.valueOf(highestChromeVersion)).setup();
-            String chromeDriverPath = WebDriverManager.chromedriver().getDownloadedDriverPath();
+            switch (browserName) {
+                case "chrome":
+                    WebDriverManager.chromedriver().browserVersion(String.valueOf(highestBrowserVersion)).setup();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().browserVersion(String.valueOf(highestBrowserVersion)).setup();
+                    break;
+                case "safari":
+                    WebDriverManager.safaridriver().browserVersion(String.valueOf(highestBrowserVersion)).setup();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().browserVersion(String.valueOf(highestBrowserVersion)).setup();
+                    break;
+            }
         } else {
             throw new RuntimeException("No devices connected");
         }
     }
 
-    private static int[] getChromeVersionsFor() {
+    public static void setChromeDriverForConnectedDevice() {
+        setDriverForConnectedDevice("chrome");
+    }
+
+    public static void setFirefoxDriverForConnectedDevice() {
+        setDriverForConnectedDevice("firefox");
+    }
+
+    public static void setSafariDriverForConnectedDevice() {
+        setDriverForConnectedDevice("safari");
+    }
+
+    public static void setEdgeDriverForConnectedDevice() {
+        setDriverForConnectedDevice("edge");
+    }
+
+    private static int[] getBrowserVersionsFor(final String browserName) {
         CommandPrompt cmd = new CommandPrompt();
         String resultStdOut = null;
-        resultStdOut = getChromeVersionsOnDevice(cmd);
+        resultStdOut = getBrowserVersionsOnDevice(cmd, browserName);
         int[] versionNamesArr = {};
         if (resultStdOut.contains("versionName=")) {
             String[] foundVersions = resultStdOut.split("\n");
             for (String foundVersion : foundVersions) {
                 String version = foundVersion.split("=")[1].split("\\.")[0];
-                String format = String.format("Found Chrome version - '%s' on device", version);
+                String format = String.format("Found " + browserName + " version - '%s' on device", version);
                 System.out.println(format);
                 versionNamesArr = ArrayUtils.add(versionNamesArr, Integer.parseInt(version));
             }
         } else {
-            System.out.println(String.format("Chrome not found on device"));
+            System.out.println(String.format(browserName + " not found on device"));
         }
         return versionNamesArr;
     }
 
-    private static String getChromeVersionsOnDevice(CommandPrompt cmd) {
+    private static String getBrowserVersionsOnDevice(CommandPrompt cmd, final String browserName) {
         String resultStdOut;
         try {
-            resultStdOut = cmd.runCommandThruProcess("adb shell dumpsys package com.android.chrome | grep versionName");
+            resultStdOut = cmd.runCommandThruProcess("adb shell dumpsys package com.android." + browserName + " | grep versionName");
         } catch (IOException e) {
             e.printStackTrace();
-            throw new RuntimeException("Error getting chrome version from device - " + e.getMessage());
+            throw new RuntimeException("Error getting " + browserName + " version from device - " + e.getMessage());
         }
         return resultStdOut;
     }
 
-    public static String getPathForFirefoxDriverFromMachine() {
+    public static String setPathForFirefoxDriverFromMachine() {
         WebDriverManager.firefoxdriver().setup();
         String firefoxDriverPath = WebDriverManager.firefoxdriver().getDownloadedDriverPath();
         System.out.println("FirefoxDriver path: " + firefoxDriverPath);
